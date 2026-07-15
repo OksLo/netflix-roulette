67c857d894b841769fd0b282fbea2304215aa521
Oks Lo <okslo@me.com>
2026-07-15 11:34:50 +0300

    - Added github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/netflix-roulette/',
   plugins: [react()],
   resolve: {
     alias: {
